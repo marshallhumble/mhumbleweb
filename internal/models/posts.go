99763bd7a2e9a4f0495d3391d854873b92e96e0c
Marshall Humble <11792578+marshallhumble@users.noreply.github.com>
2024-07-28 18:34:24 -0500

    Db working, pages loading

--- internal/models/posts.go
+++ internal/models/posts.go
@@ -2,18 +2,53 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 )
 
 type PostsInterface interface {
+	GetAll() ([]Post, error)
+	GetById(id int) (Post, error)
 }
 
 type Post struct {
 	ID      int
 	Title   string
 	Content template.HTML
+	Created string
+	Updated string
 }
 
 type PostsModel struct {
 	DB *sql.DB
+}
+
+func (pm PostsModel) GetAll() ([]Post, error) {
+	var posts []Post
+	rows, err := pm.DB.Query("SELECT * FROM posts")
+	if err != nil {
+		return posts, err
+	}
+
+	for rows.Next() {
+		var post Post
+		err = rows.Scan(&post.ID, &post.Title, &post.Content, &post.Created, &post.Updated)
+		if err != nil {
+			return posts, err
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func (pm PostsModel) GetById(id int) (Post, error) {
+	var post Post
+	if err := pm.DB.QueryRow("SELECT * FROM posts WHERE id = $1", id).Scan(&post.ID, &post.Title, &post.Content,
+		&post.Created, &post.Updated); err != nil {
+		fmt.Println(err)
+		return post, err
+	}
+
+	return post, nil
 }
