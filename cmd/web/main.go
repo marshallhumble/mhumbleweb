a308c973c09db851d031ad5cb664238d0603ed09
Marshall Humble <11792578+marshallhumble@users.noreply.github.com>
2025-04-06 19:30:29 -0500

    update fly.toml and main to publish new port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -69,6 +69,9 @@ func main() {
 	logger.Info("starting server", "addr", srv.Addr)
 	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 
-	logger.Error(err.Error())
+	if err != nil {
+		logger.Error(err.Error())
+	}
+
 	os.Exit(1)
 }
